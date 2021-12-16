5f8d312986882de1772ffb7df13c7ca76a72ed14
Nancy836 <55183861+Nancy836@users.noreply.github.com>
2021-12-17 00:44:07 +0200

    //

--- advanced_algorithms/q5.py
+++ advanced_algorithms/q5.py
@@ -1,1 +1,18 @@
-m
+def shortestReach(n, edges, s):
+
+    #collecting input for the number of nodes  and edges
+    n,m = (input('Please enter the number of nodes,  leave  space then enter the number of edges: ')).split()n
+
+    #collecting the  x,y,r  values m  times by looping through range  of m
+    for x in range(m):
+        x,y,r = (input('Please enter the beginning nodes,  leave  space then enter the ending nodes, leave  space then enter the length of the  edge: ')).split()
+
+    s = (input('Please enter starting position: ')) #collecting the value of the  starting position
+
+    shortest_path = {}#dictionary to store  the shortest paths to all the nodes  from the starting position
+
+
+
+
+
+shortestReach(0,0,0)
